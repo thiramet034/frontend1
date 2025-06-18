274095a76e24a25f5d17a2f9f64de155e4bb1925
jatuphon0949 <jatuphon070849@gmail.com>
2025-06-18 12:20:21 +0700

    first commit

--- app/about/page.js
+++ app/about/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function About() {
   return (
     <>
    <h1> Mr.Thitamet Khamja</h1>
---
+++ app/service/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Service() {
+  return (
+    <>
+   <h1>Mr.Thitamet Khamja</h1> 
+    </>
+  );
+}
